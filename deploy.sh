2c948a12cb3e64cf8c97b7bbcfd6c950f68a09de
adelina-simion-cytora <adelina.simion@cytora.com>
2021-05-06 14:51:37 +0100

    add the second param

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sam deploy --parameter-overrides "TwitterConsumerKey=$TWITTER_CONSUMER_KEY" --no-confirm-changeset --no-fail-on-empty-changeset
+sam deploy --parameter-overrides "TwitterConsumerKey=$TWITTER_CONSUMER_KEY,TwitterConsumerSecret=$TWITTER_CONSUMER_SECRET" --no-confirm-changeset --no-fail-on-empty-changeset
